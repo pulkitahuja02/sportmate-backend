0e4f25dbaecc3f99fcf769f103e4e53534a43d2f
Pulkit <ahujapulkit0202@gmail.com>
2025-04-14 22:18:30 +0530

    frontend me profile ka code dala

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://sportmate-frontend.vercel.app",  // Allow frontend domain
+  credentials: true  // Allow sending cookies/sessions
+}));
 app.use(express.json()); // ✅ Replaces body-parser, built-in in Express
 
 // Routes
